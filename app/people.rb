81736b29fd08187d135481c5e3d69d52668651cd
Sebastian Sogamoso <sebasoga@gmail.com>
2021-09-23 12:55:59 -0500

    Refactor `People`
    Moved the responsability of iterating through the formats to
    `People.build_from` to make the code a bit more readable and the class
    simpler.

--- app/people.rb
+++ app/people.rb
@@ -1,25 +1,18 @@
 class People
   def self.build_from(**formats)
-    new(**formats).build
+    formats.map do |format, raw_content|
+      new(format: format, raw_content: raw_content).build
+    end.flatten
   end
 
-  def initialize(**formats)
-    @formats = formats
+  def initialize(format:, raw_content:)
+    @format = format
+    @raw_content = raw_content
   end
 
   def build
-    build_people_from_each_format.flatten
-  end
-
-  private
-
-  attr_reader :formats
-
-  def build_people_from_each_format
-    formats.map do |format, raw_content|
-      Parsers::LIST[format].parse(raw_content).map do |row|
-        Person.new(**row.to_h)
-      end
+    Parsers::LIST[@format].parse(@raw_content).map do |parsed_row|
+      Person.new(**parsed_row.to_h)
     end
   end
 end
