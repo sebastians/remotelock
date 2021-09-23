fa3976a8cfaf745d6cdb26cecf903e34d464492b
Sebastian Sogamoso <sebasoga@gmail.com>
2021-09-23 13:22:56 -0500

    Update test for `People`

--- spec/unit/people_spec.rb
+++ spec/unit/people_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe People do
     let(:dollar_separated_values) { File.read("spec/fixtures/people_by_dollar.txt") }
     let(:percent_separated_values) { File.read("spec/fixtures/people_by_percent.txt") }
 
-    subject { described_class.build_from(dollar_separated: dollar_separated_values, percent_separated: percent_separated_values) }
+    subject { described_class.build_from(dollar_format: dollar_separated_values, percent_format: percent_separated_values) }
 
     it "returns a Person instance for each row" do
       expect(subject[0]).to be_a Person
