ec8e7391834d087dfa29bc6caf6b553c60c5f588
Sebastian Sogamoso <sebasoga@gmail.com>
2021-09-23 18:29:47 -0500

    Stop inheriting from `CSV`
    I had been debating with the idea of inheriting directly from this class
    from the stantard library. Initially I did because it felt like it made
    each of the initial parser quite powerful, upto par with `CSV`. However,
    given that we're not really using any other of its capabilities,
    inheriting from it just made the resulting class a bit more complicated
    that it needed to be.

--- app/people_parser.rb
+++ app/people_parser.rb
@@ -1,19 +1,19 @@
 require 'csv'
 
-class PeopleParser < CSV
+class PeopleParser
   SUPPORTED_FORMATS = {
     dollar_format: { separator: "$" },
     percent_format: { separator: "%" }
   }
 
-  def self.parse(string, **options)
+  def self.parse(string, format:, **options)
     custom_parse_options = {
-      col_sep: SUPPORTED_FORMATS[options.delete(:format)][:separator],
+      col_sep: SUPPORTED_FORMATS[format][:separator],
       strip: true,
       headers: true,
       header_converters: :symbol
     }
 
-    super(string, **options.merge(custom_parse_options))
+    CSV.parse(string, **options.merge(custom_parse_options))
   end
 end
