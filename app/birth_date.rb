c826dfd04cddd67e8b6693d0fd9ba6a35e795ed4
Sebastian Sogamoso <sebasoga@gmail.com>
2021-10-13 15:54:28 -0500

    Add support for dates separated by `.`

--- app/birth_date.rb
+++ app/birth_date.rb
@@ -1,6 +1,8 @@
 require "date"
 
 class BirthDate
+  PERIOD_SEPARATED_DATE_FORMAT = /\d{1,2}\.\d{1,2}\.\d{4}/
+
   def initialize(string)
     @string = string
   end
@@ -10,10 +12,22 @@ class BirthDate
   end
 
   def date
-    @date ||= Date.parse(@string)
+    @date ||= parse_date
   end
 
   def <=>(other_birth_date)
     date <=> other_birth_date.date
+  end
+
+  private
+
+  attr_reader :string
+
+  def parse_date
+    if string.match(PERIOD_SEPARATED_DATE_FORMAT)
+      Date.strptime(string.gsub(".", "-") ,"%m-%d-%Y")
+    else
+      Date.parse(string)
+    end
   end
 end
--- spec/unit/birth_date_spec.rb
+++ spec/unit/birth_date_spec.rb
@@ -1,6 +1,24 @@
 require "spec_helper"
 
 RSpec.describe BirthDate do
+  describe "#date" do
+    let(:string) { "1986-05-29" }
+
+    subject { described_class.new(string) }
+
+    it "returns a Date object from the parsed String" do
+      expect(subject.date).to eq Date.new(1986, 05, 29)
+    end
+
+    context "when the String uses periods (.) as separators" do
+      let(:string) { "1.15.1995" }
+
+      it "still returns a Date object from the parsed String" do
+        expect(subject.date).to eq Date.new(1995, 01, 15)
+      end
+    end
+  end
+
   describe "#<=>" do
     let(:newer_date) { described_class.new("2021/12/31") }
     let(:older_date) { described_class.new("2021/01/01") }
