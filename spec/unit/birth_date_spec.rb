c77e35f74e7d7f55300f0db724a82f6b2f10bcc7
Sebastian Sogamoso <sebasoga@gmail.com>
2021-10-13 15:45:27 -0500

    Improve `BirthDate#<=>` test

--- spec/unit/birth_date_spec.rb
+++ spec/unit/birth_date_spec.rb
@@ -2,12 +2,12 @@ require "spec_helper"
 
 RSpec.describe BirthDate do
   describe "#<=>" do
-    it "allows comparing two BirthDate instances" do
-      future_date = described_class.new("2021/12/31")
-      past_date = described_class.new("2021/01/01")
-      dates = [future_date, past_date]
+    let(:newer_date) { described_class.new("2021/12/31") }
+    let(:older_date) { described_class.new("2021/01/01") }
+    let(:unsorted_dates) { [newer_date, older_date] }
 
-      expect(dates.sort).to eq [past_date, future_date]
+    it "allows sorting BirthDate instances" do
+      expect(unsorted_dates.sort).to eq [older_date, newer_date]
     end
   end
 end
